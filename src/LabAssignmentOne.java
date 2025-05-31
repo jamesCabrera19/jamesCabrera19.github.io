ab555154065a6633e735b143d8d5d10e929a8233
Jaime Cabrera <66804991+jamesCabrera19@users.noreply.github.com>
2025-05-31 06:30:31 -0500

    Update LabAssignmentOne.java

--- src/LabAssignmentOne.java
+++ src/LabAssignmentOne.java
@@ -12,19 +12,26 @@ public class LabAssignmentOne {
         // number array to store user numbers
         ArrayList<Integer> numbers = new ArrayList<>();
 
-        boolean isZero = true;
+        boolean isRunning = true;
         
         // Read numbers until 0 is entered
-        while (isZero != false) {
+        while (isRunning != false) {
             System.out.print("Enter a number: ");
-            int number = usrInput.nextInt();
+            
+            if (usrInput.hasNextInt()) {
+                int number = usrInput.nextInt();
 
-            // if number is 0 we added to the array then break the loop
-            if (number == 0) {
-                numbers.add(number);
-                isZero = false;
-            } else {
-                numbers.add(number);
+                // If number is 0, add it to the array then break the loop
+                if (number == 0) {
+                    numbers.add(number);
+                    isRunning = false;
+                } else {
+                    numbers.add(number);
+                }
+            }else {
+            	usrInput.next(); 
+                System.out.println("Please enter a valid integer.");
+
             }
         }
 
@@ -45,15 +52,15 @@ public class LabAssignmentOne {
         	}else if(current < 0 ){
            	 	negativeValues++;
         	}
-        	
         	sum += numbers.get(i);
-
         }
 
         // calculating average
         float average = (float) sum / numbers.size();
 
-        System.out.println("\nSummary:");
+        System.out.println("Summary:");
+        System.out.println("---".repeat(10));
+
         System.out.println("Negative values: " + negativeValues);
         System.out.println("Positive values: " + positiveValues);
         System.out.println("Sum: " + sum);
@@ -63,6 +70,7 @@ public class LabAssignmentOne {
     }
 
     public static void main(String[] args) {
+    	// calling the method
         counter();
     }
 }
