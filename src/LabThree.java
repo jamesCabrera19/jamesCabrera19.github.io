a18b49aa199d9cb5d3b5ba33411d9bc103b23bf4
Jaime Cabrera <66804991+jamesCabrera19@users.noreply.github.com>
2025-06-03 06:27:36 -0500

    added labThree hw

---
+++ src/LabThree.java
@@ -0,0 +1,47 @@
+package one;
+
+public class LabThree {
+//	public static void bin2Dec(String binaryString) {
+//		int decimal = Integer.parseInt(binaryString, 2);
+//		
+//		System.out.println(decimal);
+//		return;
+//	}
+//	public static void main(String[] args) {
+//		System.out.println("Enter a string: " );
+//
+//		bin2Dec("hello");
+//	}
+	
+}
+
+
+class Testing{
+	public static String checkBinary(String binaryString) {
+		for(int i = 0; i < binaryString.length(); i++ ) {
+			char c = binaryString.charAt(i);
+			if(c != '0' && c != '1' ) {
+				// System.out.println(c);
+	            throw new NumberFormatException("INVALID BINARY STRING: " + binaryString);
+			}
+		}
+		return binaryString;
+	}
+	public static void bin2Dec(String binaryString) {
+		
+		String binary = checkBinary(binaryString);
+		
+		int decimal = Integer.parseInt(binary, 2);
+		System.out.println(decimal);
+		return;
+	}
+	public static void main(String[] args) {
+		
+		try {
+	        bin2Dec("1010101v101010101");  // invalid
+	        bin2Dec("1011");               // valid
+	    } catch (NumberFormatException e) {
+	        System.out.println("Error: " + e.getMessage());
+	    }
+	}
+}
